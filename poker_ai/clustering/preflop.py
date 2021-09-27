ffb4e0004d502479651a8d160efd5a5d6cd142ce
MachengShen <macshen@mit.edu>
2021-09-26 21:45:45 -0400

    remove short deck constraint

--- poker_ai/clustering/preflop.py
+++ poker_ai/clustering/preflop.py
@@ -16,36 +16,47 @@ def make_starting_hand_lossless(starting_hand, short_deck) -> int:
         suited = True
     else:
         suited = False
-    if all(c_rank == 14 for c_rank in ranks):
-        return 0
-    elif all(c_rank == 13 for c_rank in ranks):
-        return 1
-    elif all(c_rank == 12 for c_rank in ranks):
-        return 2
-    elif all(c_rank == 11 for c_rank in ranks):
-        return 3
-    elif all(c_rank == 10 for c_rank in ranks):
-        return 4
-    elif 14 in ranks and 13 in ranks:
-        return 5 if suited else 15
-    elif 14 in ranks and 12 in ranks:
-        return 6 if suited else 16
-    elif 14 in ranks and 11 in ranks:
-        return 7 if suited else 17
-    elif 14 in ranks and 10 in ranks:
-        return 8 if suited else 18
-    elif 13 in ranks and 12 in ranks:
-        return 9 if suited else 19
-    elif 13 in ranks and 11 in ranks:
-        return 10 if suited else 20
-    elif 13 in ranks and 10 in ranks:
-        return 11 if suited else 21
-    elif 12 in ranks and 11 in ranks:
-        return 12 if suited else 22
-    elif 12 in ranks and 10 in ranks:
-        return 13 if suited else 23
-    elif 11 in ranks and 10 in ranks:
-        return 14 if suited else 24
+
+    # if all(c_rank == 14 for c_rank in ranks):
+    #     return 0
+    # elif all(c_rank == 13 for c_rank in ranks):
+    #     return 1
+    # elif all(c_rank == 12 for c_rank in ranks):
+    #     return 2
+    # elif all(c_rank == 11 for c_rank in ranks):
+    #     return 3
+    # elif all(c_rank == 10 for c_rank in ranks):
+    #     return 4
+    # elif 14 in ranks and 13 in ranks:
+    #     return 5 if suited else 15
+    # elif 14 in ranks and 12 in ranks:
+    #     return 6 if suited else 16
+    # elif 14 in ranks and 11 in ranks:
+    #     return 7 if suited else 17
+    # elif 14 in ranks and 10 in ranks:
+    #     return 8 if suited else 18
+    # elif 13 in ranks and 12 in ranks:
+    #     return 9 if suited else 19
+    # elif 13 in ranks and 11 in ranks:
+    #     return 10 if suited else 20
+    # elif 13 in ranks and 10 in ranks:
+    #     return 11 if suited else 21
+    # elif 12 in ranks and 11 in ranks:
+    #     return 12 if suited else 22
+    # elif 12 in ranks and 10 in ranks:
+    #     return 13 if suited else 23
+    # elif 11 in ranks and 10 in ranks:
+    #     return 14 if suited else 24
+
+    for rank in range(2, 15):
+        if all(c_rank == rank for c_rank in ranks):
+            return 14 - rank
+
+    for rank_1 in range(14, 1, -1):
+        for rank_2 in range(rank_1 - 1, 1, -1):
+            if rank_1 in ranks and rank_2 in ranks:
+                rank = 13 + (11 + rank_1) / 2 * (14 - rank_1) + (rank_1 - rank_2 - 1)
+                return rank if suited else 78 + rank
 
 
 def compute_preflop_lossless_abstraction(builder) -> Dict[Tuple[Card, Card], int]:
@@ -54,14 +65,16 @@ def compute_preflop_lossless_abstraction(builder) -> Dict[Tuple[Card, Card], int
     Only works for the short deck presently.
     """
     # Making sure this is 20 card deck with 2-9 removed
-    allowed_ranks = {10, 11, 12, 13, 14}
-    found_ranks = set([c.rank_int for c in builder._cards])
-    if found_ranks != allowed_ranks:
-        raise ValueError(
-            f"Preflop lossless abstraction only works for a short deck with "
-            f"ranks [10, jack, queen, king, ace]. What was specified="
-            f"{found_ranks} doesn't equal what is allowed={allowed_ranks}"
-        )
+
+    # allowed_ranks = {10, 11, 12, 13, 14}
+    # found_ranks = set([c.rank_int for c in builder._cards])
+    # if found_ranks != allowed_ranks:
+    #     raise ValueError(
+    #         f"Preflop lossless abstraction only works for a short deck with "
+    #         f"ranks [10, jack, queen, king, ace]. What was specified="
+    #         f"{found_ranks} doesn't equal what is allowed={allowed_ranks}"
+    #     )
+    #
     # Getting combos and indexing with lossless abstraction
     preflop_lossless: Dict[Tuple[Card, Card], int] = {}
     for starting_hand in builder.starting_hands:
